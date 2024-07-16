03518aabe68bcd4672ac8dc5bd5efb5776ac3394
rohanvk3 <rohanvk003@gmail.com>
2024-07-16 11:49:26 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,165 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import Progress from "./Progress";
+import Footer from "./Footer";
+import NextButton from "./NextButton";
+import FinishScreen from "./FinishScreen";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          question.correctOption === action.payload
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const {
+    questions,
+    status,
+    index,
+    answer,
+    points,
+    highscore,
+    secondsRemaining,
+  } = state;
+
+  const maxPossiblePoints = questions.reduce(
+    (acc, curr) => acc + curr.points,
+    0
+  );
+
+  useEffect(function () {
+    const fetchQuestions = async () => {
+      try {
+        const res = await fetch(
+          "https://my-json-server.typicode.com/rohanvk3/server/questions"
+        );
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    };
+    fetchQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={questions.length} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={questions.length}
+              answer={answer}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                index={index}
+                numQuestions={questions.length}
+                dispatch={dispatch}
+                answer={answer}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Options.jsx
@@ -0,0 +1,25 @@
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${answer === index ? "answer" : ""} ${
+            hasAnswered
+              ? question.correctOption === index
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
